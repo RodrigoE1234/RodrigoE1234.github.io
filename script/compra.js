072bf3a8020145eb4870595ed2ba855cd208384b
RodrigoE1234 <elias.rodrigo@1hotmail.com>
2018-11-23 15:34:32 -0300

    detalles
    Reformando

--- script/compra.js
+++ script/compra.js
@@ -5,18 +5,25 @@ document.addEventListener("DOMContentLoaded", function(){
     var radioDigital = document.getElementById("digital");
     var totalEnvio = document.getElementById("total-envio");
 
+/*var numb = 123.34535;
+numb = numb.toFixed(2); 
+*/
+
     radioFisico.addEventListener("change", function(){
         divFormato.style.display = "block";
     });
     radioDigital.addEventListener("change", function(){
         divFormato.style.display = "none";
         costoEnvio = 0;
+        totalEnvio.innerHTML = costoEnvio;
     }); 
     
     /* Precios */
     var precioJuego = 619;
     var costoEnvio = 0;
     var subtotal = 0;
+    var num;
+    var cant
 
     var campoCantidad = document.getElementById("cantidad");
     var encabezadoSubtotal = document.getElementById("subtotal");
@@ -30,22 +37,43 @@ document.addEventListener("DOMContentLoaded", function(){
         var cantidad = campoCantidad.value;
         cantidad = parseInt(cantidad);
         subtotal = cantidad * precioJuego;
-        encabezadoSubtotal.innerHTML = subtotal;
-        encabezadoTotal.innerHTML = subtotal * 1.22;
+        encabezadoSubtotal.innerHTML = subtotal.toFixed(2);
+        num = subtotal * 1.22;
+        encabezadoTotal.innerHTML = num.toFixed(2);
     }); 
     envioComun.addEventListener("change", function(){
+        var cantidad = campoCantidad.value;
+        cantidad = parseInt(cantidad);
+        subtotal = cantidad * precioJuego;
+        
         costoEnvio = subtotal * 0.005;
         totalEnvio.innerHTML = costoEnvio;
         encabezadoSubtotal.innerHTML = subtotal + costoEnvio;
+        num = subtotal * 1.22 + costoEnvio;
+        encabezadoTotal.innerHTML = num.toFixed(2);
     });
     envioEspecial.addEventListener("change", function(){
+        var cantidad = campoCantidad.value;
+        cantidad = parseInt(cantidad);
+        subtotal = cantidad * precioJuego;
+
         costoEnvio = subtotal * 0.02;
         totalEnvio.innerHTML = costoEnvio;
         encabezadoSubtotal.innerHTML = subtotal + costoEnvio;
+        num = subtotal * 1.22 + costoEnvio;
+        encabezadoTotal.innerHTML = num.toFixed(2);
+
     });
     envioPremium.addEventListener("change", function(){
+        var cantidad = campoCantidad.value;
+        cantidad = parseInt(cantidad);
+        subtotal = cantidad * precioJuego;
+
         costoEnvio = subtotal * 0.05;
-        totalEnvio.innerHTML = costoEnvio;
+        cant = subtotal * 0.05;
+        totalEnvio.innerHTML = cant.toFixed(2);
         encabezadoSubtotal.innerHTML = subtotal + costoEnvio;
+        num = subtotal * 1.22 + costoEnvio;
+        encabezadoTotal.innerHTML = num.toFixed(2);
     });
 }); 
